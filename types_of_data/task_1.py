a563fff8fef8fadf5fe7c69b750e4c1d20a7f5fb
unknown <sushilovvladislav@gmail.com>
2026-03-11 12:30:22 +0300

    Tasks type of data added

---
+++ types_of_data/task_1.py
@@ -0,0 +1,21 @@
+# Задание 1
+
+a  = 42
+b = 3.14159
+c = "Hello, Python!"
+d = True
+e = [1, 2, 3]
+f = (4, 5, 6)
+g = {"name": "Alice", "age": 30}
+h = {7, 8, 9}
+i = None
+
+print(type(a)) # Целое число
+print(type(b)) # Число с плавающей точкой
+print(type(c)) # Строка
+print(type(d)) # Булевый тип
+print(type(e)) # Лист
+print(type(f)) # Кортеж
+print(type(g)) # Словарь
+print(type(h)) # Множество
+print(type(i)) # Тип данных None
